9c93fdec45ec71ed188ed497bdf746edf848fd54
Alex TEA <alextea2@gmail.com>
2019-12-14 01:10:44 +0100

    fix fetch if GET request

--- src/utils.js
+++ src/utils.js
@@ -6,10 +6,13 @@ export const api = (url, body, method) => {
   if (jwt) {
     headers.authorization = `Bearer ${jwt}`
   }
-  return fetch(`http://localhost:4000${url}`, {
-    method: method,
-    body: JSON.stringify(body),
-    headers: headers
-  })
+  const options = {
+    method,
+    headers
+  };
+  if (body !== null) {
+    options.body = JSON.stringify(body)
+  }
+  return fetch(`http://localhost:4000${url}`, options)
   .then(res => res.json())
 };
