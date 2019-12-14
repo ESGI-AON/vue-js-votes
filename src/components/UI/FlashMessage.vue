80e85aa5e55dceced77a8edc6eea0e6f6c373f63
Alex TEA <alextea2@gmail.com>
2019-12-14 01:19:22 +0100

    add flash message component

---
+++ src/components/UI/FlashMessage.vue
@@ -0,0 +1,19 @@
+<template>
+    <p class="inline-block mx-auto my-4 px-4 py-3 rounded-lg">{{ this.message }}</p>
+</template>
+
+<script>
+  export default {
+    name: "FlashMessage",
+    props: ['message'],
+  }
+</script>
+
+<style scoped>
+  .success{
+    @apply bg-green-200
+  }
+  .error {
+    @apply bg-red-200
+  }
+</style>
