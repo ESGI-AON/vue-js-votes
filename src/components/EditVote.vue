bbcb9c29eac89b422bf3cf5c4271a7669eb7061a
Alex TEA <alextea2@gmail.com>
2019-12-14 14:55:28 +0100

    edit vote

--- src/components/EditVote.vue
+++ src/components/EditVote.vue
@@ -1,58 +1,70 @@
 <template>
-  <div class="w-2/3 mx-auto">
-                          <h1 class="font-hairline mb-6 text-center uppercase font-bold text-2xl pt-2">Edit vote</h1>
-
-    <div class="bg-white shadow-md rounded my-6">
-      <table class="text-left w-full border-collapse">
-
-        <!--Border collapse doesn't work on this site yet but it's available in newer tailwind versions -->
-        <thead>
-          <tr>
-            <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-900 border-b border-gray-100">Title
-            </th>
-            <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-900 border-b border-gray-100">
-              Description
-            </th>
-
-            <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-900 border-b border-gray-100">Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="hover:bg-gray-100">
-            <td class="py-4 px-6 border-b border-gray-100">MyVote1</td>
-            <td class="py-4 px-6 border-b border-gray-100">It's my first vote</td>
-
-            <td class="py-4 px-6 border-b border-gray-100">
-              <a href="#"
-                class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-green-999 hover:bg-green-400 ">Edit</a> 
-              <a href="#"
-                class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-blue-900 hover:bg-blue-400">View</a>
-            </td>
-          </tr>
-          <tr class="hover:bg-gray-100">
-            <td class="py-4 px-6 border-b border-gray-100">MyVote2</td>
-            <td class="py-4 px-6 border-b border-gray-100">It's my first vote</td>
-            <td class="py-4 px-6 border-b border-gray-100">
-              <a href="#"
-                class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-green-999 hover:bg-green-400">Edit</a>
-              <a href="#"
-                class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-blue-900 hover:bg-blue-400">View</a>
-            </td>
-          </tr>
-          <tr class="hover:bg-gray-100">
-            <td class="py-4 px-6 border-b border-gray-100">MyVote3</td>
-            <td class="py-4 px-6 border-b border-gray-100">It's my first vote</td>
-            <td class="py-4 px-6 border-b border-gray-100">
-              <a href="#"
-                class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-green-999 hover:bg-green-400">Edit</a>
-              <a href="#"
-                class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-blue-900 hover:bg-blue-400">View</a>
-            </td>
-          </tr>
-          
-        </tbody>
-      </table>
-    </div>
+  <div>
+    <Formik @onSubmit="editVote" :initial-values="initialValues">
+      <FlashMessage v-if="isSuccessful" :message="message" class="success"/>
+      <FormGroup v-for="field in fields"
+        :name="field.name"
+        :type="field.type"
+        :label="field.label"
+        :value="field.value"
+      />
+    </Formik>
   </div>
 </template>
+
+<script>
+  import FormGroup from "./Form/FormGroup";
+  import Formik from "./Form/Formik";
+  import FlashMessage from "./UI/FlashMessage";
+  import {api} from "../utils"
+
+  export default {
+    name: 'EditVote',
+    components: {
+      Formik,
+      FormGroup,
+      FlashMessage
+    },
+    data(){
+      return {
+        fields: {
+          title: {
+            type: 'text',
+            name: 'title',
+            label: 'Title',
+            value: ''
+          },
+          desc: {
+            type: 'text',
+            name: 'desc',
+            label: 'Description',
+            value: ''
+          },
+        },
+        initialValues: {},
+        isSuccessful: false,
+        message: "You successfuly edit the vote"
+      }
+    },
+    mounted() {
+      this.getVote()
+    },
+    methods: {
+      getVote(){
+        api(`/votes/${this.$route.params.uuid}`, null, 'GET')
+        .then(vote => {
+          Object.entries(vote).forEach(([key, value]) => {
+            if (this.fields[key]) {
+              this.fields[key]['value'] = value
+              this.initialValues[key] = value
+            }
+          })
+        })
+      },
+      editVote(body){
+        api(`/votes/${this.$route.params.uuid}`, body, 'PUT')
+        .then(() => this.isSuccessful = true)
+      }
+    }
+  }
+</script>
